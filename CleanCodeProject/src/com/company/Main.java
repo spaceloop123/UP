3f0368fb07f6373f7f17f8831b7e0ddcbd86c012
Sanya <spaceloop123@outlook.com>
2016-02-09 21:00:43 +0300

    Test commit

--- CleanCodeProject/src/com/company/Main.java
+++ CleanCodeProject/src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Gson gson = new Gson();
+        gson.toJson("Test", System.out);
     }
 }
