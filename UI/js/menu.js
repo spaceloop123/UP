c020491a0f747e3934268369089b67e83f8b7dee
Sanya <spaceloop123@outlook.com>
2016-04-11 07:44:39 +0200

    UI 1.2.3

--- UI/js/menu.js
+++ UI/js/menu.js
@@ -3,68 +3,53 @@
 /*
 Side menu in mobile and tablet version
 */
-  var layout   = document.getElementById('layout'),
-  menu     = document.getElementById('menu'),
-  menuLink = document.getElementById('menuLink');
+var layout   = document.getElementById('layout'),
+menu     = document.getElementById('menu'),
+menuLink = document.getElementById('menuLink');
 
-  menuLink.addEventListener("click", function (e) {
-    e.preventDefault();
+menuLink.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    toggleClass(layout, "active");
-    toggleClass(menu, "active");
-    toggleClass(menuLink, "active");
-  });
+  toggleClass(layout, "active");
+  toggleClass(menu, "active");
+  toggleClass(menuLink, "active");
+});
+
 /*
-Edit username 
+Test
 */
-  var isEdit = false;
-  
-  var menuBtn = document.getElementById("btn-group");
-  menuBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+var isEdit = false;
 
-    if(isEdit) {
-      toggleClass(document.getElementById("card"), "flipped");
-      removeClass(document.getElementById("btn-container"), "edit");
-      toggleClass(document.getElementById("plus-icon"), "hidden");
-      toggleClass(document.getElementById("plus-icon"), "rotate");
-      toggleClass(document.getElementById("check-icon"), "hidden");
+var fab = document.getElementById("fab");
+var close = document.getElementById("close");
+var textInputName = document.getElementById("text-input-name");
+var myName = document.getElementById("myname-edit");
 
-      user.textContent = document.getElementById("myname-edit").value;
+var user = document.getElementById("profile-name");
 
-      isEdit = false;
-    } else {
-      toggleClass(document.getElementById("btn-container"), "active");
-      toggleClass(document.getElementById("plus-icon"), "rotate");
-    }
-  });
+fab.addEventListener("click", function(e) {
+  e.preventDefault();
 
-  var editBtn = document.getElementById("edit-btn");
-  var myName = document.getElementById("myname-edit");
+  addClass(fab, "active");
+  addClass(close, "active");
+  addClass(textInputName, "active");
 
-  var user = document.getElementById("profile-name");
+  isEdit = true;
 
-  editBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+  document.getElementById("myname-edit").focus();
+  document.getElementById("myname-edit").value = user.textContent;
+});
 
-    if (!isEdit) {
-      toggleClass(document.getElementById("card"), "flipped");
-      removeClass(document.getElementById("btn-container"), "active");
-      addClass(document.getElementById("btn-container"), "edit");
-      toggleClass(document.getElementById("plus-icon"), "hidden");
-      toggleClass(document.getElementById("check-icon"), "hidden");
-      isEdit = true;
-      
-      document.getElementById("myname-edit").focus();
-      document.getElementById("myname-edit").value = user.textContent;
+close.addEventListener("click", function(e) {
+  e.preventDefault();
 
-    } else {
-      toggleClass(document.getElementById("card"), "flipped");
-      removeClass(document.getElementById("btn-container"), "edit");
-      toggleClass(document.getElementById("plus-icon"), "hidden");
-      toggleClass(document.getElementById("plus-icon"), "rotate");
-      toggleClass(document.getElementById("check-icon"), "hidden");
-    }
-  });
+  removeClass(fab, "active");
+  removeClass(close, "active");
+  removeClass(textInputName, "active");
+
+  user.textContent = document.getElementById("myname-edit").value;
+
+  isEdit = false;
+});
 
 }(this, this.document));
